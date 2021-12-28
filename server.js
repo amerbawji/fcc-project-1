9bd104de33389364cb9e23ff9612e1986ec2442c
Amer <amerbawji@gmail.com>
2021-12-28 18:28:36 +0200

    finished prj 2

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+var database_uri = 'mongodb+srv://amerbawji:YYOf6a7TMDbesfrj@cluster0.zp6w1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // server.js
 // where your node app starts
 
 // init project
 var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var shortid = require('shortid');
 var app = express();
 var port = process.env.PORT || 3000; //to stop running the server on a dynamic port let it be fixed on 3000
+
+// mongoose.connect(process.env.DB_URI)
+mongoose.connect(database_uri, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,6 +36,10 @@ app.get("/timestamp", function (req, res) {
 
 app.get("/requestHeaderParser", function (req, res) {
   res.sendFile(__dirname + '/views/requestHeaderParser.html');
+});
+
+app.get("/urlShortenerMicroservice", function (req, res) {
+  res.sendFile(__dirname + '/views/urlShortenerMicroservice.html');
 });
 
 // your first API endpoint... 
@@ -71,6 +86,47 @@ app.get("/api/whoami", function(req,res){
       "software": req.headers["user-agent"]
     });
   });
+
+
+var ShortURL = mongoose.model('ShortURL', new mongoose.Schema({
+  short_url: String,
+  original_url: String,
+  suffix: String
+}));
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.post("/api/shorturl/", function(req,res){
+  let client_requested_url = req.body.url
+  let suffix = shortid.generate();
+  let newShortURL = suffix
+  console.log(suffix, "<= this will be our suffix");
+
+  let newURL = new ShortURL({
+    short_url: __dirname + "/api/shorturl" + suffix,
+    original_url: client_requested_url,
+    suffix: suffix
+  })
+  newURL.save( function(err, doc){
+    if (err) return console.error(err);
+    console.log("successfully saved!");
+    res.json({
+      "saved": true,
+      "short_url": newURL.short_url,
+      "original-url": newURL.original_url,
+      "suffix": newURL.suffix
+    });
+  });
+});
+
+app.get("/api/shorturl/:suffix", function(req,res){
+  let userGeneratedSuffix = req.params.suffix;
+  ShortURL.find({suffix: userGeneratedSuffix}).then(function(foundUrls){
+    let urlForRedirect = foundUrls[0];
+    res.redirect(urlForRedirect.original_url);
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(port, function () {
